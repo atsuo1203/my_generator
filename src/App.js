398dcfa06a330fd1c29dc396dda46ab32f358272
atsuo1203 <dondonkitemasu@gmail.com>
2018-04-27 16:32:25 +0900

    簡易遷移完了

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom'
 import './App.css';
 import BanjoLittle from './BanjoLittle';
+import BanjoFull from './BanjoFull';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <BanjoLittle/>
+        <BrowserRouter>
+          <div>
+            <ul>
+              <li><Link to='/little'>簡易バージョン</Link></li>
+              <li><Link to='/full'>フルバージョン</Link></li>
+            </ul>
+            <hr />
+            <Route exact path='/little' component={BanjoLittle} />
+            <Route exact path='/full' component={BanjoFull} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/BanjoFull.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class BanjoFull extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name:"",
+      verb:"",
+      inputText: "",
+    }
+  }
+
+  handleChangeName = event =>  {
+    this.setState({name: event.target.value});
+  }
+
+  handleChangeVerb = event =>  {
+    this.setState({verb: event.target.value});
+  }
+
+  handleMakeWord = () => {
+    var resultText = "「お前が"+this.state.name+"を"+this.state.verb+
+                     "のは勝手だ。けどそうなった場合、誰が代わりに"+this.state.name+"をすると思う？」\n"+
+                     "「…」\n"+
+                     "「万丈だ。」\n"
+    this.setState({inputText: resultText})
+  }
+
+  render() {
+    return (
+      <div className="BanjoFull">
+        <h2>フルバージョン</h2>
+        <label>
+          名詞:
+        </label>
+        <input type="text" name="name" onChange={this.handleChangeName} placeholder="代表戦" />
+        <br/>
+        <label>
+          動詞:
+        </label>
+        <input type="text" name="verb" onChange={this.handleChangeVerb} placeholder="出ない"/>
+        <br/>
+        <button type="button" name="makeButton" onClick={this.handleMakeWord}>
+          作成
+        </button>
+        <br/>
+        <textarea name="kanso" rows="10" cols="50" value= {this.state.inputText} />
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default BanjoFull;
